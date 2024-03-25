cc63de5ae25256035c33b56226d51cd22a13f75a
Le Van Kiem <levankiemdev@gmail.com>
2024-03-25 16:20:36 +0700

    LOGIN API

--- src/controller/loginController.js
+++ src/controller/loginController.js
@@ -1,14 +1,6 @@
 const getLoginPage = (req, res) => {
-  const arrMesage = req.flash("data");
-
-  const error = arrMesage[0] ? arrMesage[0] : "";
-  const usernameInput = arrMesage[1] ? arrMesage[1] : "";
-  return res.render("login.ejs", {
-    error: error,
-    usernameInput: usernameInput,
-  });
+  return res.render("login.ejs");
 };
-
 module.exports = {
   getLoginPage,
 };
